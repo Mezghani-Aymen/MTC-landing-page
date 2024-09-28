f272aef75143f01d714e62436ee33a77e19f8f2b
unknown <mezghanimohamedaymen@gmail.com>
2024-09-28 18:59:48 +0100

    [fix] create blog section

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ $(document).ready(function () {
         loop: true,
         margin: 20,
         nav: true,
-        autoplay: true,
+        autoplay: false,
         autoplayTimeout: 3000,
         autoplayHoverPause: true,
         responsive: {
