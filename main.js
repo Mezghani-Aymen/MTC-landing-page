2be0efffc1a05a1c26281b0ef44ec707fb099411
unknown <mezghanimohamedaymen@gmail.com>
2024-09-28 17:41:32 +0100

    [fix] create Team section

--- main.js
+++ main.js
@@ -4,15 +4,36 @@ const menuList = document.getElementById('menu-list');
 
 menuList.addEventListener('click', (event) => {
     const targetId = event.target.getAttribute('data-target');
-    // if (targetId) {
     const targetSection = document.getElementById(targetId);
     if (targetSection) {
         targetSection.scrollIntoView({ behavior: 'smooth' });
     }
-    // }
     menu.classList.toggle('hidden');
 });
 
 menuButton.addEventListener('click', () => {
     menu.classList.toggle('hidden');
+});
+
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        loop: true,
+        margin: 20,
+        nav: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
+        responsive: {
+            600: {
+                items: 2
+            },
+            768: {
+                items: 3
+            },
+            1200: {
+                items: 4
+            }
+        }
+    });
 });
